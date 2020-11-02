7a43f0fbac624feefc172d31bf9efb4bae4700cb
sarahlia <sarahn@rightnowmedia.org>
2020-11-02 15:28:42 -0600

    refactored code to create an instance fo address block FormGroup in a method.

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -56,14 +56,7 @@ export class CustomerComponent implements OnInit {
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       sendCatalog: true,
-      addresses: this.fb.group({
-        addressType: 'home',
-        street1: '',
-        street2: '',
-        city: '',
-        state: '',
-        zip: ''
-      })
+      addresses: this.buildAddress()
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(
@@ -76,6 +69,17 @@ export class CustomerComponent implements OnInit {
     ).subscribe(
       value => this.setMessage(emailControl)
     );
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
   }
 
   populateTestData(): void {
